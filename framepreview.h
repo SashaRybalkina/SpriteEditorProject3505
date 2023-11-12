85353c47c1a27d0531d2287935c40f0792e3e767
ALawnMower <alanzhmo@gmail.com>
2023-11-11 23:04:24 -0700

    added to framepreview.h

--- framepreview.h
+++ framepreview.h
@@ -2,6 +2,12 @@
 #define FRAMEPREVIEW_H
 
 #include <QWidget>
+#include <QTimer>
+#include <QStackedWidget>
+#include <QGraphicsScene>
+#include <QCheckBox>
+#include <QSlider>
+#include <QSpinBox>
 
 namespace Ui {
 class FramePreview;
@@ -15,8 +21,24 @@ public:
     explicit FramePreview(QWidget *parent = nullptr);
     ~FramePreview();
 
+public slots:
+    void togglePlayPause();
+    void changeFPS(int fps);
+    void toggleActualSize();
+
 private:
     Ui::FramePreview *ui;
+    QTimer timer;
+    QStackedWidget* frameStack;
+    QGraphicsScene* scene;
+
+    bool isPlaying = false;
+    bool atActualSize = false;
+    int currentFPS = 12;
+
+    QSlider* fpsSlider;
+    QSpinBox* fpsBox;
+    QCheckBox* actualSizeCheckbox;
 };
 
 #endif // FRAMEPREVIEW_H
